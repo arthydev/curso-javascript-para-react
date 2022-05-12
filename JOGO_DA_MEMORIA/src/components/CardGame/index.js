c7eb717ccdcc36dc6352e739e993e36e0ee97c61
Arthur Catelani <arthurhccomercial@gmail.com>
2022-05-11 21:36:02 -0300

    Create CardGame Component

---
+++ JOGO_DA_MEMORIA/src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function CardGame () {
+    return `
+        <article class="Card-Game">
+            <img src="img/alura-pixel.png">
+            <p class="name">Alura</p>
+        </article>
+    `
+}
+
+
+export default CardGame
